5ad4ab3f37cb0de052704cba684847f84b815636
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-10-04 13:02:05 +0100

    Test-driving CRUD routes challenge

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -3,16 +3,64 @@ require "rack/test"
 require_relative '../../app'
 
 describe Application do
-  # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
 
-  # We need to declare the `app` value by instantiating the Application
-  # class so our tests work.
   let(:app) { Application.new }
+
+  context 'testing GET ./albums' do
+    it 'should return the list of albums' do
+      response = get('/albums')
+
+      expected_response = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
 
   context 'POST ./albums' do
     it 'should create a new album' do
-      
+      response = post(
+        '/albums', 
+        title: 'Voyage', 
+        release_year: '2022', 
+        artist_id: '2'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/albums')
+
+      expect(response.body).to include('Voyage')
+    end
+  end
+
+  context 'testing GET ./artists' do
+    it 'should return the list of artists' do
+      response = get('/artists')
+
+      expected_response = 'Pixies, ABBA, Taylor Swift, Nina Simone'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context 'POST ./artists' do
+    it 'should create a new artist' do
+      response = post(
+        '/artists', 
+        name: 'Wild Nothing', 
+        genre: 'Indie'
+      )
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('')
+
+      response = get('/artists')
+
+      expect(response.body).to include('Wild Nothing')
     end
   end
 end
