bbaab105d2321bdfc9b6a377664fe95b71e2da99
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-10-04 10:52:28 +0100

    Cloned Directory

---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'POST ./albums' do
+    it 'should create a new album' do
+      
+    end
+  end
+end
